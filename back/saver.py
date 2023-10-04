61d9466356142afa390006ae5f52a279efd2775e
BraisIglesias17 <braisiglesias17@gmail.com>
2023-10-04 21:16:36 +0200

    Saver object

---
+++ back/saver.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+class FileSaver(ABC):
+    @abstractmethod
+    def save(self, filename, content):
+        pass
+
+class DataSetSaver(FileSaver):
+
+    def save(self,filename,content):
+         if not isinstance(content,pd.DataFrame):
+            raise ValueError("Must be an pandas dataframe") 
+         else:
+             self._save(filename,content)
+    @abstractmethod
+    def _save(self,filename,content):
+        pass
+
+class CSVFileSaver(DataSetSaver):
+    def _save(self, filename, content):
+        content.to_csv(filename,index=False)
+
+class XLSFileSaver(DataSetSaver):
+    def _save(self, filename, content):
+        content.to_excel(filename,index=False)
+        
+class TextFileSaver(FileSaver):
+    def save(self, filename, content):
+        with open(filename, 'w') as file:
+                file.write(content) 
+
+
+class Saver():
+    def __init__(self,path,content):
+        self.saver=None
+        self.path=path
+        self.content=content
+        pathname=str(path).replace("\\","/")
+        if str(pathname).endswith(".xlsx"):
+            self.saver=XLSFileSaver()
+        elif str(pathname).endswith(".csv"):
+            self.saver=CSVFileSaver()
+        elif str(pathname).endswith(".txt"):
+            self.saver=TextFileSaver()
+    
+    def save(self):
+        self.saver.save(self.path,self.content)
+            
