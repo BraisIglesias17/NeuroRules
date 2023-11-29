fbc0a497a3308c20a22405af04d3036baab3590a
BraisIglesias17 <braisiglesias17@gmail.com>
2023-11-29 22:13:02 +0100

    refactored

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
+""" Module that starts the application. """
+import sys
 import wx
 from front.views.mainWindow import MainWindow
-import sys
+
 
 if getattr(sys, 'frozen', False):
     import pyi_splash
 
 if __name__ == '__main__':
-
     print("---------------------------------- ")
-    
     print("------- Starting NEURORULE ------- ")
-    
     app = wx.App()
-    
     frame = MainWindow(None, wx.ID_ANY, "")
-
     if getattr(sys, 'frozen', False):
         pyi_splash.close()
-        
     app.MainLoop()
-
     print("Exiting ... ")
     print("---------------------------------- ")
-    
-    
-
-
-
-
